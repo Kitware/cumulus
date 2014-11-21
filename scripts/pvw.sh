201b101b3e6af40a4401d3c941c359ca72664e6c
Chris Harris <chris.harris@kitware.com>
2014-11-21 13:44:52 -0500

    Update pvm submit script

--- scripts/pvw.sh
+++ scripts/pvw.sh
@@ -6,7 +6,7 @@
 ###   run.sh
 ###
 #$ -S /bin/bash
-#$ -pe orte {{ number_of_slots }}
+#$ -q all.q@master
 
 # Set up cluster-specific variables
 PARAVIEW_DIR="/opt/paraview/install"
@@ -26,9 +26,12 @@ IPADDRESS=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
 # Create proxy entry
 BODY='{"host": "'$IPADDRESS'", "clusterId": "{{ cluster._id }}", "port": 8080, "jobId": "{{ job._id }}"}'
-curl -X PATCH -d "$BODY"  --header "Content-Type: application/json" {{ base_url }}/proxy
+curl -X POST -d "$BODY"  --header "Content-Type: application/json" {{ base_url }}/proxy
 
 export LD_LIBRARY_PATH=$PARAVIEW_DIR/lib/paraview-4.2
 
 # First run pvpython with the reverse connect port
 ${PV_PYTHON} ${VISUALIZER} --host $IPADDRESS --port 8080
+
+# Remove proxy entry
+curl -X DELETE {{ base_url }}/proxy/{{ cluster._id }}/{{ job._id }}
