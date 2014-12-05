c58ea0489c169e8e22a6fdbad7be442dda38339e
Chris Harris <chris.harris@kitware.com>
2014-12-05 22:44:00 +0000

    Set the celery fetch limit

--- cumulus/starcluster/tasks/celery.py
+++ cumulus/starcluster/tasks/celery.py
@@ -13,6 +13,8 @@ app = Celery('starcluster',  backend='amqp', broker='amqp://guest:guest@localhos
 app.conf.update(
     CELERY_TASK_SERIALIZER='json',
     CELERY_ACCEPT_CONTENT=('json',),
-    CELERY_RESULT_SERIALIZER = 'json'
+    CELERY_RESULT_SERIALIZER = 'json',
+    CELERY_ACKS_LATE=True,
+    CELERYD_PREFETCH_MULTIPLIER=1
 )
 
