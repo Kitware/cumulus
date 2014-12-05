b12b4a77f18fb8b7f9e3e86563b6135631fcfed6
Chris Harris <chris.harris@kitware.com>
2014-12-05 19:35:43 +0000

    Add data-dir to PVW script

--- scripts/pvw.sh
+++ scripts/pvw.sh
@@ -17,6 +17,8 @@ VISUALIZER="${PARAVIEW_DIR}/${APPS_DIR}/pv_web_visualizer.py"
 RC_PORT="54321"
 REVERSE="--reverse-connect-port ${RC_PORT}"
 PROXIES="config/defaultProxies.json"
+DATA="data/"
+
 
 # Get the private ip of this host
 IPADDRESS=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
@@ -28,7 +30,7 @@ curl --silent --show-error -o /dev/null -X POST -d "$BODY"  --header "Content-Ty
 export LD_LIBRARY_PATH=$PARAVIEW_DIR/lib/paraview-4.2
 
 # First run pvpython with the reverse connect port
-${PV_PYTHON} ${VISUALIZER} --host $IPADDRESS --port 8080 --proxies ${PROXIES} ${REVERSE}
+${PV_PYTHON} ${VISUALIZER} --host $IPADDRESS --port 8080 --proxies ${PROXIES} ${REVERSE} --data-dir ${DATA}
 
 # Remove proxy entry
 curl --silent --show-error -o /dev/null -X DELETE {{ base_url }}/proxy/{{ cluster._id }}/{{ job._id }}
