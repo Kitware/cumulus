c487faaf3a92c9b0a56af4034997cd16d86db27b
Chris Harris <chris.harris@kitware.com>
2015-09-08 15:44:08 -0400

    We need to special case awsProfile.name
    As awsProfile.name is used as a key we can't have a dot in it. So
    render awsProfile_name for this purpose.

--- girder/cumulus/server/starclusterconfig.py
+++ girder/cumulus/server/starclusterconfig.py
@@ -213,6 +213,10 @@ class StarClusterConfig(BaseResource):
             json_str = json.dumps(config)
             json_str = Template(json_str) \
                 .render(awsProfile=profile,
+                        # We need to profile the separatly as we can't have
+                        # dot in mongo keys and need to use awsProfile name
+                        # as a key.
+                        awsProfile_name=profile['name'],
                         keyStore=cumulus.config.ssh.keyStore)
             config = json.loads(json_str)
 
