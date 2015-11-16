86648e7bb2f66f3d2176adc620e2af9d563cdb23
Christopher Kotfila <chris.kotfila@kitware.com>
2015-11-16 14:01:16 -0500

    Adding test for ClusterStatus

---
+++ girder/cumulus/plugin_tests/ansible_test.py
@@ -0,0 +1,63 @@
+import mock
+import json
+import cherrypy
+from tests import base
+from cumulus.constants import ClusterStatus
+
+
+def setUpModule():
+    base.enabledPlugins.append('cumulus')
+    cherrypy.server.socket_port = 8081
+    base.startServer(mock=False)
+
+
+def tearDownModule():
+    base.stopServer()
+
+
+class AnsibleTestCase(base.TestCase):
+
+    def setUp(self):
+        super(AnsibleTestCase, self).setUp()
+        users = ({
+            'email': 'cumulus@email.com',
+            'login': 'cumulus',
+            'firstName': 'First',
+            'lastName': 'Last',
+            'password': 'goodpassword'
+        }, {
+            'email': 'regularuser@email.com',
+            'login': 'regularuser',
+            'firstName': 'First',
+            'lastName': 'Last',
+            'password': 'goodpassword'
+        })
+
+        self._cumulus, self._user = \
+            [self.model('user').createUser(**user) for user in users]
+
+        self._group = self.model('group').createGroup('cumulus', self._cumulus)
+
+    def test_cluster_status(self):
+        names = [s.name for s in ClusterStatus]
+        self.assertTrue("error" in names)
+        self.assertTrue("creating" in names)
+        self.assertTrue("created" in names)
+        self.assertTrue("deploying" in names)
+        self.assertTrue("deployed" in names)
+        self.assertTrue("provisioning" in names)
+        self.assertTrue("provisioned" in names)
+        self.assertTrue("stopped" in names)
+        self.assertTrue("running" in names)
+
+        self.assertTrue(ClusterStatus.error < ClusterStatus.creating)
+        self.assertTrue(ClusterStatus.creating < ClusterStatus.created)
+        self.assertTrue(ClusterStatus.created < ClusterStatus.deploying)
+        self.assertTrue(ClusterStatus.deploying < ClusterStatus.deployed)
+        self.assertTrue(ClusterStatus.deployed < ClusterStatus.provisioning)
+        self.assertTrue(ClusterStatus.provisioning < ClusterStatus.provisioned)
+        self.assertTrue(ClusterStatus.provisioned < ClusterStatus.stopped)
+        self.assertTrue(ClusterStatus.stopped < ClusterStatus.running)
+
+    def test_create(self):
+        pass
