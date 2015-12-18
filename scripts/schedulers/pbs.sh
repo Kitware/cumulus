dc86b33bfb37fb33cc3aa44b20c8fb5fac4478b8
Chris Harris <chris.harris@kitware.com>
2015-12-18 10:42:05 -0500

    Allow more job submission options for PBS

--- scripts/schedulers/pbs.sh
+++ scripts/schedulers/pbs.sh
@@ -2,6 +2,8 @@
 #PBS -N {{job.name}}-{{job._id}}
 {% if numberOfSlots -%}
 #PBS -l procs={{numberOfSlots}}
+{% elif numberOfNodes -%}
+#PBS -l nodes={{numberOfNodes}}{{':ppn=%s' % numberOfCoresPerNode if numberOfCoresPerNode}}{{':gpus=%s' % numberOfGpusPerNode if numberOfGpusPerNode}}
 {% endif -%}
 {% if maxWallTime -%}
 #PBS -l walltime={{maxWallTime}}
