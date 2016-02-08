a872f47281dd0d7c60b1abb922320c69c1e9f92d
Chris Harris <chris.harris@kitware.com>
2016-02-08 13:20:56 -0500

    Fix flake8 issues

--- cumulus/taskflow/utility/__init__.py
+++ cumulus/taskflow/utility/__init__.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright 2016 Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
 import os
 import pkgutil
 import pkg_resources as pr
@@ -5,21 +23,23 @@ import sys
 
 import cumulus
 
+
 def find_modules(paths, prefix=''):
     for (loader, name, pkg) in pkgutil.iter_modules(paths):
         if pkg:
             package_dir = os.path.join(loader.path, name)
-            for module in find_modules([package_dir], prefix='%s%s.' % (prefix, name)):
+            for module in find_modules([package_dir], prefix='%s%s.' % (prefix,
+                                                                        name)):
                 yield module
         else:
             yield '%s%s' % (prefix,  name)
 
+
 def find_taskflow_modules():
     # There has to be a better way!
-    base_path = os.path.dirname(
-                    pr.resource_filename(cumulus.__name__, '__init__.py'))
-    base_path = os.path.abspath(
-                    os.path.join(base_path, '..'))
+    base_path = os.path.dirname(pr.resource_filename(cumulus.__name__,
+                                                     '__init__.py'))
+    base_path = os.path.abspath(os.path.join(base_path, '..'))
     paths = []
     if 'taskflow' not in cumulus.config:
         print 'WARN: No taskflow path set.'
