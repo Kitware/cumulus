08189b88bed669fb036fe4a908ff81338471cea9
Chris Harris <chris.harris@kitware.com>
2016-06-14 07:56:51 -0400

    Python3: Ensure request body is encode as JSON

--- girder/cumulus/plugin_tests/cluster_test.py
+++ girder/cumulus/plugin_tests/cluster_test.py
@@ -518,7 +518,7 @@ class ClusterTestCase(base.TestCase):
         job_id = r.json['_id']
 
         r = self.request('/clusters/%s/job/%s/submit' % (str(cluster_id), str(job_id)), method='PUT',
-                         type='application/json', body={}, user=self._user)
+                         type='application/json', body=json.dumps({}), user=self._user)
         expected_response = {
             u'message': u'Cluster is not running', u'type': u'rest'}
         self.assertEqual(r.json, expected_response)
@@ -537,7 +537,7 @@ class ClusterTestCase(base.TestCase):
         self.assertStatusOk(r)
 
         r = self.request('/clusters/%s/job/%s/submit' % (str(cluster_id), str(job_id)), method='PUT',
-                         type='application/json', body={}, user=self._user)
+                         type='application/json', body=json.dumps({}), user=self._user)
         self.assertStatusOk(r)
 
         expected_submit_call = \
@@ -796,7 +796,7 @@ class ClusterTestCase(base.TestCase):
         self.assertStatus(r, 201)
         _id = str(r.json['_id'])
         r = self.request('/clusters/%s/terminate' % _id, method='PUT',
-                         type='application/json', body={}, user=self._user)
+                         type='application/json', body=json.dumps({}), user=self._user)
 
         self.assertStatus(r, 400)
 
