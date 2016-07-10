f14a6d0fe7657ae13e53cc4f4edc1acb09da7078
TristanWright <tristan.wright@kitware.com>
2016-07-10 11:06:25 +0200

    removed newline in taskflow/plugin.cmake

--- girder/taskflow/plugin.cmake
+++ girder/taskflow/plugin.cmake
@@ -1,4 +1,3 @@
 add_python_test(task PLUGIN taskflow)
 add_python_test(taskflow PLUGIN taskflow)
-add_python_style_test(python_static_analysis_taskflow
-"${PROJECT_SOURCE_DIR}/plugins/taskflow/server")
+add_python_style_test(python_static_analysis_taskflow "${PROJECT_SOURCE_DIR}/plugins/taskflow/server")
