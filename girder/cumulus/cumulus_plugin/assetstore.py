08ecc702b8913bcdb3532f0c2872e9b5fe81d2d4
Alessandro Genova <alessandro.genova@kitware.com>
2020-02-12 09:37:15 -0500

    Ensure only admins can access sensitive assetstore info

--- girder/cumulus/cumulus_plugin/assetstore.py
+++ girder/cumulus/cumulus_plugin/assetstore.py
@@ -1,11 +1,21 @@
 from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
+from girder.api.rest import getCurrentUser
+from girder.constants import TokenScope
 from girder.models.assetstore import Assetstore
 
-@access.user
+@access.user(scope=TokenScope.DATA_READ)
 @autoDescribeRoute(
     Description('Retrieve assetstores with a given name.')
     .param('name', 'The assetstore name', required=True)
     .errorResponse())
 def lookupAssetstore(name, params):
-    return Assetstore().find({'name': name})
+    assetstores = Assetstore().find({'name': name})
+    user = getCurrentUser()
+    if user.get('admin', False):
+        return assetstores
+    else:
+        safe_only = lambda assetstore : {
+            field: assetstore[field] for field in ('_id', 'name')
+        }
+        return list(map(safe_only, assetstores))
